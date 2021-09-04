8b7d6cc895d457fb7eec37634928261f8d24ad56
SanskritiPandey <khushiksp17@gmail.com>
2021-09-04 14:02:08 -0700

    feat :add event listener

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("script is working");
+// alert("this is the alert script")
+// var username = prompt("give me your username ")
+// alert("welcome!"+ username)
+
+var btnTrans = document.querySelector("#btn-trans")
+// console.log(btnTrans)
+
+// add event listener
+btnTrans.addEventListener("click",function clickevenhandler(){
+    console.log("clicked") })
+
