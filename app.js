229bc1d6da418f9d2f18b08ebcb96ef310566b91
SanskritiPandey <khushiksp17@gmail.com>
2021-09-04 14:25:35 -0700

    feat: adding input and console

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log("script is working");
+// console.log("script is working");
 // alert("this is the alert script")
 // var username = prompt("give me your username ")
 // alert("welcome!"+ username)
 
 var btnTrans = document.querySelector("#btn-trans")
 // console.log(btnTrans)
-
+function clickevenhandler(){
+    console.log("clicked") 
+console.log("input",inputTxt.value)}
 // add event listener
-btnTrans.addEventListener("click",function clickevenhandler(){
-    console.log("clicked") })
+btnTrans.addEventListener("click", clickevenhandler)
 
+var inputTxt = document.querySelector("#txt")
+// console.log(inputTxt)
