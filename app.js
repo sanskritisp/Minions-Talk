c440dea1c676ca4c3a2069cdea9f1bce1f8df0ac
SanskritiPandey <khushiksp17@gmail.com>
2021-09-04 15:29:58 -0700

    feat: adding input and output

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 // alert("this is the alert script")
 // var username = prompt("give me your username ")
 // alert("welcome!"+ username)
-
+var outputText = document.querySelector("#output")
+const inputTxt = document.querySelector("#txt")
 var btnTrans = document.querySelector("#btn-trans")
 // console.log(btnTrans)
 function clickevenhandler(){
-    console.log("clicked") 
-console.log("input",inputTxt.value)}
+    outputText.innerText = "Translated into banana "+ inputTxt.value
+
+
+//     console.log("clicked") 
+// console.log("input", inputTxt.value)
+}
+
 // add event listener
 btnTrans.addEventListener("click", clickevenhandler)
 
-var inputTxt = document.querySelector("#txt")
-// console.log(inputTxt)
+
